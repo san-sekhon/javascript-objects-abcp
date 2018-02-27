fe6f047044eb3c79eb14293251116d7c16f6c6a5
Sandeep Sekhon <sandysss79@gmail.com>
2018-02-27 05:51:19 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,12 @@
+var playlist = {
+  kishore: 'evergreen',
+  lata: 'duet'
+}
+function updatePlaylist(obj, artist, song) {
+  obj[artist] = song
+  return obj
+}
+function removeFromPlaylist(obj, artist) {
+  delete obj[artist]
+  return obj
+}
